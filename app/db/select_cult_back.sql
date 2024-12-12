1966ec74b7a1a0dbb935c3374e3a290c95dcad7c
Алексей Лыгалов <lygalov@tbdd.ru>
2024-12-12 11:49:51 +0500

    @new рефакторинг, добавлена sqlite3 БД, с общим Life_way

---
+++ app/db/select_cult_back.sql
@@ -0,0 +1,12 @@
+SELECT 
+    CULT_BACK.name AS region_name,   -- Имя региона
+    LANG.name AS language_name      -- Имя языка
+FROM 
+    REGION_LANGUAGES
+JOIN 
+    CULT_BACK ON REGION_LANGUAGES.region_id = CULT_BACK.id   -- Соединение с таблицей регионов
+JOIN 
+    LANG ON REGION_LANGUAGES.language_id = LANG.id 	-- Соединение с таблицей языков
+WHERE CULT_BACK.name = 'Южная Азия'
+ORDER BY 
+    CULT_BACK.name, LANG.name;  -- Сортировка по имени региона и имени языка
